3d0c62062c27bfac781d87fd9cc140978a37c577
cnzyan <cnzyan@qq.com>
2025-05-19 22:17:51 +0800

    重构依赖检查脚本，优化编码检测和网站可用性检查逻辑

--- check-requirements1.py
+++ check-requirements1.py
@@ -1,8 +1,7 @@
 import subprocess
-import urllib.request
+import urllib3
 from collections import defaultdict
 import unicodedata
- 
 def detect_encoding(byte_str):
     encodings = ['utf-16','utf-8', 'gbk', 'ascii', 'iso-8859-1', 'cp1252']
     probs = defaultdict(int)
@@ -18,9 +17,19 @@ def detect_encoding(byte_str):
         return [k for k, v in probs.items() if v == max_prob]  # 返回概率最高的编码列表，可能有多个相同概率的编码。
     return None
 
-# 执行命令并获取输出
-result = subprocess.run(['pip', 'list'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+http = urllib3.PoolManager()
  
+def is_site_up(url):
+    try:
+        response = http.request('HEAD', url)
+        return response.status == 200
+    except urllib3.exceptions.MaxRetryError:
+        return False
+    
+# 执行命令并获取输出
+result = subprocess.run(
+    ['pip', 'list'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
 # 打印输出
 # print(result.stdout)
 whllist = result.stdout.split('\n')
@@ -33,7 +42,7 @@ for whl in whllist:
 # 如果有错误，也可以打印错误信息
 if result.stderr:
     print(result.stderr)
-    
+
 # 获取返回码
 exit_code = result.returncode
 
@@ -47,26 +56,25 @@ with open('requirements.txt', 'r',encoding=encoding) as f:
     # print(requirements)
     for i in requirements:
         if i not in whllist1:
-            #print(i)
-            #print('not install')
+            # print(i)
+            # print('not install')
             lacklist.append(i)
         else:
-            #print(i)
-            #print('install')
+            # print(i)
+            # print('install')
             pass
 # print(lacklist)
-
-pysources=['https://pypi.tuna.tsinghua.edu.cn/simple','https://mirrors.aliyun.com/pypi/simple/']
-pys_url=pysources[0]
+pysources = ['https://pypi.tuna.tsinghua.edu.cn/simple',
+             'https://mirrors.aliyun.com/pypi/simple/']
+pys_url = pysources[0]
 for pys in pysources:
-	# print(pys)
-	try:
-		status=urllib.request.urlopen(pys).status
-		if status==200:
-			pys_url=pys
-			break
-	except:
-		continue
+    # print(pys)
+    try:
+        if is_site_up(pys):
+            pys_url = pys
+            break
+    except:
+        continue
 if len(lacklist) == 0:
     print('all requirements are installed')
 else:
